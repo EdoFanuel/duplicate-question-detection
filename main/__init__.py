5a3a7367bdd221f38f5c52917cdde60a37c1956c
EdoFanuel <edofanuel.siswanto@yahoo.com>
2017-05-25 21:40:18 +0700

    - fix wrong file name

--- main/__init__.py
+++ main/__init__.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
     test_file = "..\\dataset\\test.csv"
     test_dict_file = "..\\feature\\quora_testset.dict"
-    test_feature_file = "..\\feature\test_feature.csv"
+    test_feature_file = "..\\feature\\test_feature.csv"
     file_name = "..\\result\\{0}.csv".format(dt.now().strftime("%Y-%m-%d %H-%M-%S"))
 
     # Generating training features
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         train_feature = generate_feature(train_file, train_dict_file, train_feature_file)
 
     # Initialize random forest
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(max_depth=5)
     clf.fit(train_feature[f_ext.FeatureExtraction.get_feature_fields()], train_feature["is_duplicate"])
 
     # Generating test features
@@ -59,13 +59,12 @@ if __name__ == '__main__':
         test_feature = generate_feature(test_file, test_dict_file, test_feature_file)
 
     # Find result
-    prediction = clf.predict_proba(test_feature[f_ext.FeatureExtraction.get_feature_fields()])
-    final_result = []
-    for row in test_feature.iterrows():
-        final_result.append({
-            "test_id": row["id"],
-            "is_duplicate": prediction[row["id"]][1]
-        })
+    print("Predicting values...")
+    preds = clf.predict_proba(test_feature[f_ext.FeatureExtraction.get_feature_fields()])
+    print("Saving result")
+    final_result = pd.DataFrame()
+    final_result["test_id"] = test_feature["id"]
+    final_result["is_duplicate"] = [pred[1] for pred in preds]
 
     # Save result
-    fmgmt.write_csv(final_result, file_name)
+    final_result.to_csv(file_name, index=False)
